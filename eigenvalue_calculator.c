9c79cdf1873a13a4f4ca660c00380582e623d16b
Madhav Kataria <156478369+madhavkataria14@users.noreply.github.com>
2024-03-17 14:49:08 +0530

    Update eigenvalue_calculator.c

--- eigenvalue_calculator.c
+++ eigenvalue_calculator.c
@@ -1,117 +1,91 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
-#include <time.h>
+#include <string.h>
+#include <gsl/gsl_matrix.h>
+#include <gsl/gsl_vector.h>
+#include <gsl/gsl_multifit.h>
 
-#define MAX_SIZE 10
+#define MAX_ROWS 1000
+#define MAX_COLS 100
 
-void get_matrix_dimensions(int *rows, int *cols) {
-    printf("Enter the number of rows: ");
-    scanf("%d", rows);
-    printf("Enter the number of columns: ");
-    scanf("%d", cols);
+int read_csv(const char *filename, gsl_matrix *data) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        printf("Error opening file\n");
+        return 0;
+    }
+
+    int rows = 0, cols = 0;
+    char line[MAX_COLS * 10]; // Assuming a maximum line length
+
+    while (fgets(line, sizeof(line), file)) {
+        char *token = strtok(line, ",");
+        int col = 0;
+        while (token != NULL) {
+            gsl_matrix_set(data, rows, col, atof(token));
+            token = strtok(NULL, ",");
+            col++;
+        }
+        if (cols == 0) cols = col;
+        rows++;
+    }
+
+    fclose(file);
+    return rows;
 }
 
-void get_matrix_elements(double matrix[][MAX_SIZE], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("Enter element at [%d][%d]: ", i, j);
-            scanf("%lf", &matrix[i][j]);
-        }
+double sigmoid(double x, double a, double b) {
+    return 1.0 / (1.0 + exp(-(a * x + b)));
+}
+
+void plot_graph(gsl_vector *coefficients, gsl_matrix *data) {
+    for (int i = 0; i < data->size1; i++) {
+        double x = gsl_matrix_get(data, i, 0);
+        double y = gsl_matrix_get(data, i, 1);
+        printf("%lf %lf\n", x, y);
     }
 }
 
-void generate_random_matrix(double matrix[][MAX_SIZE], int rows, int cols) {
-    srand(time(NULL));
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = (double)rand() / RAND_MAX; 
-        }
-    }
-}
-
-void print_matrix(double matrix[][MAX_SIZE], int rows, int cols) {
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            printf("%lf ", matrix[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-double power_iteration(double matrix[][MAX_SIZE], int rows, int cols) {
-    double eigenvector[MAX_SIZE];
-    double eigenvalue = 0.0;
-    int iterations = 0;
-
-    for (int i = 0; i < rows; i++) {
-        eigenvector[i] = (double)rand() / RAND_MAX;
-    }
-
-    while (iterations < 100) { 
-        double new_eigenvector[MAX_SIZE];
-
-        for (int i = 0; i < rows; i++) {
-            new_eigenvector[i] = 0.0;
-            for (int j = 0; j < cols; j++) {
-                new_eigenvector[i] += matrix[i][j] * eigenvector[j];
-            }
-        }
-
-        double norm = 0.0;
-        for (int i = 0; i < rows; i++) {
-            norm += new_eigenvector[i] * new_eigenvector[i];
-        }
-        norm = sqrt(norm);
-        for (int i = 0; i < rows; i++) {
-            new_eigenvector[i] /= norm;
-        }
-
-        for (int i = 0; i < rows; i++) {
-            eigenvector[i] = new_eigenvector[i];
-        }
-        eigenvalue = 0.0;
-        for (int i = 0; i < rows; i++) {
-            eigenvalue += matrix[i][i] * eigenvector[i] * eigenvector[i];
-        }
-
-        iterations++;
-    }
-
-    return eigenvalue;
-}
-
-int main() {
-    int rows, cols;
-    double matrix[MAX_SIZE][MAX_SIZE];
-
-    printf("Eigenvalue Calculator\n");
-
-    int choice;
-    printf("1. Enter matrix manually\n");
-    printf("2. Generate random matrix\n");
-    printf("Enter your choice: ");
-    scanf("%d", &choice);
-
-    if (choice == 1) {
-        get_matrix_dimensions(&rows, &cols);
-        get_matrix_elements(matrix, rows, cols);
-    } else if (choice == 2) {
-        printf("Enter the number of rows: ");
-        scanf("%d", &rows);
-        printf("Enter the number of columns: ");
-        scanf("%d", &cols);
-        generate_random_matrix(matrix, rows, cols);
-    } else {
-        printf("Invalid choice!\n");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <csv_file>\n", argv[0]);
         return 1;
     }
 
-    printf("\nThe matrix is:\n");
-    print_matrix(matrix, rows, cols);
+    gsl_matrix *data = gsl_matrix_alloc(MAX_ROWS, MAX_COLS);
+    int rows = read_csv(argv[1], data);
 
-    double eigenvalue = power_iteration(matrix, rows, cols);
-    printf("\nDominant Eigenvalue: %lf\n", eigenvalue);
+    if (rows == 0) {
+        printf("No data found in file\n");
+        gsl_matrix_free(data);
+        return 1;
+    }
+
+    gsl_vector *coefficients = gsl_vector_alloc(2);
+    gsl_matrix *covariance = gsl_matrix_alloc(2, 2);
+    double chisq;
+
+    gsl_vector *x = gsl_vector_alloc(rows);
+    gsl_vector *y = gsl_vector_alloc(rows);
+
+    for (int i = 0; i < rows; i++) {
+        gsl_vector_set(x, i, gsl_matrix_get(data, i, 0));
+        gsl_vector_set(y, i, gsl_matrix_get(data, i, 1));
+    }
+
+    gsl_multifit_linear_workspace *work = gsl_multifit_linear_alloc(rows, 2);
+    gsl_multifit_linear(x, y, coefficients, covariance, &chisq, work);
+    gsl_multifit_linear_free(work);
+
+    printf("# x y\n");
+    plot_graph(coefficients, data);
+
+    gsl_matrix_free(data);
+    gsl_vector_free(coefficients);
+    gsl_matrix_free(covariance);
+    gsl_vector_free(x);
+    gsl_vector_free(y);
 
     return 0;
 }
+
